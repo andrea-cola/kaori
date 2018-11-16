14a490ab0d739254b6c3111f810101f9b07ef3ea
andrea-cola <andrea.cola95@gmail.com>
2018-11-16 17:55:32 +0100

    Andrea Cola Aggiornati i valori dei colori e delle stringhe nelle resources. Implementato il metodo che verifica se l'utente si è già loggato nell'app e la chiamata al layout corretto sulla base della risposta del metodo appena citato.

--- app/src/main/java/com/kaori/kaori/Kaori.java
+++ app/src/main/java/com/kaori/kaori/Kaori.java
@@ -1,16 +1,28 @@
 package com.kaori.kaori;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+/**
+ * Entry point of the app.
+ */
 public class Kaori extends AppCompatActivity {
 
+    /**
+     * Variables.
+     */
     private TextView mTextMessage;
 
+    /**
+     * Listener used to handle selections in the bottom bar.
+     * Each branch of the switch handle the selection of one icon in the bar.
+     */
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -20,25 +32,43 @@ public class Kaori extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_hub:
+                    mTextMessage.setText(R.string.title_hub);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_study_with_me:
+                    mTextMessage.setText(R.string.title_study_with_me);
+                    return true;
+                case R.id.navigation_my_profile:
+                    mTextMessage.setText(R.string.title_my_profile);
                     return true;
             }
             return false;
         }
     };
 
+    /**
+     * On create method override.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(checkLoginStatus()) {
+            setContentView(R.layout.activity_main);
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        }
+        else
+            setContentView(R.layout.login1);
+    }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    /**
+     * This method checks if the user has already logged in the app.
+     * If yes returns true, otherwise false.
+     */
+    private boolean checkLoginStatus(){
+        SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = app_preferences.edit();
+        return app_preferences.getBoolean("master_login", false);
     }
 
 }
