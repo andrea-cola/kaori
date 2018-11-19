44e27f9f11507302ae610350880e7026d48e5f6a
andrea-cola <andrea.cola95@gmail.com>
2018-11-19 23:30:25 +0100

    Commit after manual merge.

--- app/src/main/java/com/kaori/kaori/Kaori.java
+++ app/src/main/java/com/kaori/kaori/Kaori.java
@@ -8,23 +8,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.graphics.Color;
 
-<<<<<<< HEAD
-import com.kaori.kaori.BottomBarActivities.BookFragment;
-import com.kaori.kaori.BottomBarActivities.FeedFragment;
-import com.kaori.kaori.DBObjects.Book;
-=======
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
->>>>>>> 4724c100effa5bde91cc3cd14f76c551bc35b1bc
+import com.kaori.kaori.BottomBarActivities.FeedFragment;
 import com.kaori.kaori.LoginRegistrationFragments.LoginRegistrationFragment;
-import static android.support.constraint.Constraints.TAG;
 
 /**
  * Entry point of the app.
@@ -72,15 +62,10 @@ public class Kaori extends AppCompatActivity implements FragmentManager.OnBackSt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-<<<<<<< HEAD
-        if(! checkLoginStatus()) {
-=======
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
 
-        if(checkLoginStatus()) {
->>>>>>> 4724c100effa5bde91cc3cd14f76c551bc35b1bc
-
+        if(!checkLoginStatus()) {
             setContentView(R.layout.activity_main);
             BottomNavigationView navigation = findViewById(R.id.navigation);
             navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
