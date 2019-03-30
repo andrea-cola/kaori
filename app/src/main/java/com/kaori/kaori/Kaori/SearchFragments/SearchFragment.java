72229a1e962bfe86351480c0b055d037bfbce916
andrea-cola <andrea.cola95@gmail.com>
2019-03-30 11:22:18 +0100

    Andrea --> searchview design update.

--- app/src/main/java/com/kaori/kaori/Kaori/SearchFragments/SearchFragment.java
+++ app/src/main/java/com/kaori/kaori/Kaori/SearchFragments/SearchFragment.java
@@ -13,42 +13,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kaori.kaori.App;
+import com.kaori.kaori.Constants;
 import com.kaori.kaori.Kaori.HomeFragments.MaterialFragment;
 import com.kaori.kaori.Model.Document;
 import com.kaori.kaori.R;
 import com.kaori.kaori.Services.DataManager;
+import com.kaori.kaori.Services.LogManager;
 
 import java.util.List;
 
-/**
- * Search Fragment is used to search materials.
- */
 public class SearchFragment extends Fragment {
 
-    /**
-     * Constants
-     */
     private final String BACK_STATE_NAME = getClass().getName();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.search_fragment_layout, container, false);
-        view.findViewById(R.id.empty_view).setVisibility(View.VISIBLE);
 
         RecyclerView recyclerView = view.findViewById(R.id.searchList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new RecyclerAdapter(DataManager.getInstance().getSearchElements()));
 
+        App.setEmptyView(view.findViewById(R.id.empty_view));
+
         SearchView searchView = view.findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                ((TextView)view.findViewById(R.id.empty_view_text)).setText(R.string.empty_text_search);
-                App.setEmptyView(view.findViewById(R.id.empty_view));
+                view.findViewById(R.id.none_view).setVisibility(View.GONE);
+                LogManager.getInstance().printConsoleMessage("print");
                 DataManager.getInstance().queryMaterials(query, recyclerView);
                 hideKeyboard(view);
                 return false;
@@ -56,6 +54,10 @@ public class SearchFragment extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String s) {
+                if(s.equalsIgnoreCase("")) {
+                    view.findViewById(R.id.none_view).setVisibility(View.VISIBLE);
+                    App.setAuxiliarViewsStatus(Constants.NO_VIEW_ACTIVE);
+                }
                 return false;
             }
 
@@ -65,13 +67,11 @@ public class SearchFragment extends Fragment {
     }
 
     private void hideKeyboard(View view) {
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        if(getActivity() != null)
+            ((InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE))
+                    .hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
-    /**
-     * This method invokes the book fragment when the card is clicked
-     */
     private void invokeFragment(Fragment fragment) {
         if(getActivity()!= null) {
             getActivity().getSupportFragmentManager().beginTransaction()
@@ -86,7 +86,7 @@ public class SearchFragment extends Fragment {
 
         private List<Document> materials;
 
-        /*package-private*/ RecyclerAdapter(List<Document> materials){
+        RecyclerAdapter(List<Document> materials){
             this.materials = materials;
         }
 
@@ -98,8 +98,29 @@ public class SearchFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull final RecyclerAdapter.Holder holder, int i) {
-            holder.setDetails(materials.get(i).getTitle(), materials.get(i).getUser().getName());
-            holder.mView.setOnClickListener(view -> {
+            holder.title.setText(materials.get(i).getTitle());
+            holder.author.setText(materials.get(i).getUser().getName());
+
+            String s = materials.get(i).getExams().get(0);
+            for(int j = 1; j < materials.size(); j++)
+                s = s + ", " + materials.get(i).getExams().get(j);
+            holder.exams.setText(s);
+
+            if(materials.get(i).getSubtype() == Constants.BOOK){
+                holder.docIcon.setVisibility(View.GONE);
+                holder.linkIcon.setVisibility(View.GONE);
+                holder.bookIcon.setVisibility(View.VISIBLE);
+            } else if(materials.get(i).getSubtype() == Constants.FILE){
+                holder.bookIcon.setVisibility(View.GONE);
+                holder.linkIcon.setVisibility(View.GONE);
+                holder.docIcon.setVisibility(View.VISIBLE);
+            } else {
+                holder.docIcon.setVisibility(View.GONE);
+                holder.bookIcon.setVisibility(View.GONE);
+                holder.linkIcon.setVisibility(View.VISIBLE);
+            }
+
+            holder.view.setOnClickListener(view -> {
                 MaterialFragment materialFragment = new MaterialFragment();
                 materialFragment.setMaterial(materials.get(i));
                 invokeFragment(materialFragment);
@@ -111,27 +132,22 @@ public class SearchFragment extends Fragment {
             return materials.size();
         }
 
-        /**
-         * Private adapter for the RecyclerView
-         */
-        /*package-private*/ class Holder extends RecyclerView.ViewHolder {
-            private View mView;
-            private TextView title, author, course;
+        class Holder extends RecyclerView.ViewHolder {
+            private View view;
+            private TextView title, author, exams;
+            private ImageView docIcon, linkIcon, bookIcon;
 
             private Holder(View itemView) {
                 super(itemView);
-                mView = itemView;
+                view = itemView;
+                title = view.findViewById(R.id.title);
+                exams = view.findViewById(R.id.exams);
+                author = view.findViewById(R.id.owner);
+                docIcon = view.findViewById(R.id.label_doc);
+                linkIcon = view.findViewById(R.id.label_url);
+                bookIcon = view.findViewById(R.id.label_book);
             }
 
-            private void setDetails(String t, String a){ //, String c){
-                title = mView.findViewById(R.id.title);
-                author = mView.findViewById(R.id.author);
-                //course = mView.findViewById(R.id.course);
-
-                title.setText(t);
-                author.setText(a);
-                //course.setText(c);
-            }
         }
     }
 
